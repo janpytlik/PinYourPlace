add27377a6da7d8871cbed28a8be06110f26e1ab
info@janpytlik.cz <hanes@.(none)>
2011-04-26 21:34:00 +0200

    Layout for settings page

--- gen/com/jetminds/pinyourplace/R.java
+++ gen/com/jetminds/pinyourplace/R.java
@@ -26,12 +26,27 @@ public final class R {
         public static final int icon=0x7f02000c;
     }
     public static final class id {
+        public static final int automaticLabel=0x7f050013;
+        public static final int automaticSyncButton=0x7f050014;
+        public static final int buttonBox=0x7f05000e;
+        public static final int credentialsLabel=0x7f050006;
         public static final int description=0x7f050003;
         public static final int goButton=0x7f050004;
         public static final int historyTabLayout=0x7f050002;
+        public static final int loginButton=0x7f05000f;
         public static final int main=0x7f050000;
         public static final int mapview=0x7f050001;
+        public static final int passBox=0x7f05000a;
+        public static final int passLabel=0x7f05000b;
+        public static final int passText=0x7f05000c;
+        public static final int registerButton=0x7f050010;
         public static final int settingsTabLayout=0x7f050005;
+        public static final int statusLabel=0x7f05000d;
+        public static final int syncBox=0x7f050012;
+        public static final int synchronizationLabel=0x7f050011;
+        public static final int userBox=0x7f050007;
+        public static final int usernameLabel=0x7f050008;
+        public static final int usernameText=0x7f050009;
     }
     public static final class layout {
         public static final int displaymap=0x7f030000;
