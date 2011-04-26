788cbeef0561675a66f9939ce52006f9a3776255
Ludek Kopacek <ludek.kopacek@jetminds.com>
2011-04-26 19:45:06 +0200

    History activity

--- gen/com/jetminds/pinyourplace/R.java
+++ gen/com/jetminds/pinyourplace/R.java
@@ -14,13 +14,15 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int goButton=0x7f050002;
+        public static final int goButton=0x7f050003;
         public static final int main=0x7f050000;
         public static final int mapview=0x7f050001;
+        public static final int myTableLayout=0x7f050002;
     }
     public static final class layout {
         public static final int displaymap=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int historylistview=0x7f030001;
+        public static final int main=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
