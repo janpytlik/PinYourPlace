19b3c8f78f307e1264eac99f6bb0289e5c23a9d6
Ludek Kopacek <ludek.kopacek@jetminds.com>
2011-05-03 17:06:28 +0200

    merge

--- gen/com/jetminds/pinyourplace/R.java
+++ gen/com/jetminds/pinyourplace/R.java
@@ -11,21 +11,52 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int go_button=0x7f020000;
+        public static final int go_button_normal=0x7f020001;
+        public static final int go_button_pressed=0x7f020002;
+        public static final int ic_tab_history=0x7f020003;
+        public static final int ic_tab_history_selected=0x7f020004;
+        public static final int ic_tab_history_unselected=0x7f020005;
+        public static final int ic_tab_home=0x7f020006;
+        public static final int ic_tab_home_selected=0x7f020007;
+        public static final int ic_tab_home_unselected=0x7f020008;
+        public static final int ic_tab_settings=0x7f020009;
+        public static final int ic_tab_settings_selected=0x7f02000a;
+        public static final int ic_tab_settings_unselected=0x7f02000b;
+        public static final int icon=0x7f02000c;
     }
     public static final class id {
-        public static final int goButton=0x7f050002;
+        public static final int goButton=0x7f050003;
         public static final int main=0x7f050000;
         public static final int mapview=0x7f050001;
+        public static final int myTableLayout=0x7f050002;
     }
     public static final class layout {
         public static final int displaymap=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int historylistview=0x7f030001;
+        public static final int main=0x7f030002;
+
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int db_create_table_location=0x7f04000e;
+        public static final int db_create_table_trip=0x7f04000f;
+        public static final int db_location_created=0x7f04000d;
+        public static final int db_location_lat=0x7f04000b;
+        public static final int db_location_lon=0x7f04000c;
+        public static final int db_location_trip=0x7f04000a;
+        /**  Init DB 
+         */
+        public static final int db_name=0x7f040005;
+        public static final int db_table_location=0x7f040006;
+        public static final int db_table_trip=0x7f040007;
+        public static final int db_trip_desc=0x7f040009;
+        public static final int db_trip_name=0x7f040008;
         public static final int goButton=0x7f040002;
         public static final int hello=0x7f040000;
         public static final int map=0x7f040003;
+        /**  Default values 
+         */
+        public static final int unsaved_trip=0x7f040004;
     }
 }
